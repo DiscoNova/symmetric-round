e5464f0c70ef4c04988ec87e2a2b436948529f13
Markku Uttula <markku.uttula@cleverest.eu>
2019-07-11 13:06:41 +0300

    Function uses Math.sign() instead of ternary

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-const symmetricRound = (x) => Math.round(Math.abs(x)) * (x < 0 ? -1 : 1);
+const symmetricRound = (x) => Math.round(Math.abs(x)) * Math.sign(x);
 
 module.exports = {
     symmetricRound,
